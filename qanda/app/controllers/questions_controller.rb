5904d1ce87514e92e7bce4e124d1df60f92ff6d1
Kotarou Ogino <kotapapa101029@yahoo.co.jp>
2026-05-03 19:33:35 +0900

    add initial mvc&route

--- qanda/app/controllers/questions_controller.rb
+++ qanda/app/controllers/questions_controller.rb
@@ -1,2 +1,25 @@
 class QuestionsController < ApplicationController
+  #質問一覧表示
+  def index
+  end
+
+  #質問詳細表示
+  def show
+  end
+
+  #質問新規作成
+  def new
+  end
+
+  #質問編集
+  def edit
+  end
+
+  #質問更新
+  def update
+  end
+
+  #質問削除
+  def destroy
+  end
 end
